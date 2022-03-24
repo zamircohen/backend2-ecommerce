f1fb51581636477fbb71eaed339f5586f0c2ea33
Christoffer Ekeroth <ekeroth.christoffer@gmail.com>
2022-03-24 13:19:03 +0100

    Update dummy data

--- client/src/App.js
+++ client/src/App.js
@@ -4,38 +4,78 @@ import Product from './components/Product.js';
 function App() {
   const products = [{
     name: "Fancy product",
+    sku: "ABC-123",
     price: 899,
+    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
+    Praesentium at dolorem quidem modi. Nam sequi consequatur obcaecati
+    excepturi alias magni, accusamus eius blanditiis delectus ipsam minima ea
+    iste laborum vero?`,
     image: "https://dummyimage.com/450x300/dee2e6/6c757d.jpg"
   }, {
     name: "New product",
+    sku: "DEF-456",
     price: 749,
+    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
+    Praesentium at dolorem quidem modi. Nam sequi consequatur obcaecati
+    excepturi alias magni, accusamus eius blanditiis delectus ipsam minima ea
+    iste laborum vero?`,
     image: "https://dummyimage.com/450x300/dee2e6/6c757d.jpg"
   }, {
     name: "Discounted product",
+    sku: "GHI-789",
     price: 599,
     discountPrice: 379,
+    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
+    Praesentium at dolorem quidem modi. Nam sequi consequatur obcaecati
+    excepturi alias magni, accusamus eius blanditiis delectus ipsam minima ea
+    iste laborum vero?`,
     image: "https://dummyimage.com/450x300/dee2e6/6c757d.jpg"
   }, {
     name: "Cheap product",
+    sku: "JKL-012",
     price: 199,
+    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
+    Praesentium at dolorem quidem modi. Nam sequi consequatur obcaecati
+    excepturi alias magni, accusamus eius blanditiis delectus ipsam minima ea
+    iste laborum vero?`,
     image: "https://dummyimage.com/450x300/dee2e6/6c757d.jpg"
   }, {
     name: "Another discounted product",
+    sku: "MNO-345",
     price: 499,
     discountPrice: 399,
+    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
+    Praesentium at dolorem quidem modi. Nam sequi consequatur obcaecati
+    excepturi alias magni, accusamus eius blanditiis delectus ipsam minima ea
+    iste laborum vero?`,
     image: "https://dummyimage.com/450x300/dee2e6/6c757d.jpg"
   }, {
     name: "Super Fancy product",
+    sku: "PQR-678",
     price: 999,
+    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
+    Praesentium at dolorem quidem modi. Nam sequi consequatur obcaecati
+    excepturi alias magni, accusamus eius blanditiis delectus ipsam minima ea
+    iste laborum vero?`,
     image: "https://dummyimage.com/450x300/dee2e6/6c757d.jpg"
   }, {
     name: "Another new product",
+    sku: "STU-901",
     price: 419,
+    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
+    Praesentium at dolorem quidem modi. Nam sequi consequatur obcaecati
+    excepturi alias magni, accusamus eius blanditiis delectus ipsam minima ea
+    iste laborum vero?`,
     image: "https://dummyimage.com/450x300/dee2e6/6c757d.jpg"
   }, {
     name: "Last year's model",
+    sku: "VWX-234",
     price: 599,
     discountPrice: 379,
+    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
+    Praesentium at dolorem quidem modi. Nam sequi consequatur obcaecati
+    excepturi alias magni, accusamus eius blanditiis delectus ipsam minima ea
+    iste laborum vero?`,
     image: "https://dummyimage.com/450x300/dee2e6/6c757d.jpg"
   }];
   return (
