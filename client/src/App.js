3713d7e75d4022844cfbdc12c736b045458a65b5
Christoffer Ekeroth <ekeroth.christoffer@gmail.com>
2022-03-24 13:12:30 +0100

    Fix typo

--- client/src/App.js
+++ client/src/App.js
@@ -72,7 +72,7 @@ function App() {
             <div className="container px-4 px-lg-5 my-5">
                 <div className="text-center text-white">
                     <h1 className="display-4 fw-bolder">Shop in style</h1>
-                    <p className="lead fw-normal text-white-50 mb-0">With this shop hompeage template</p>
+                    <p className="lead fw-normal text-white-50 mb-0">With this shop homepage template</p>
                 </div>
             </div>
         </header>
