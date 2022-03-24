14b2ad44de9ccddb2f5487e3079e39ec50788e46
Christoffer Ekeroth <ekeroth.christoffer@gmail.com>
2022-03-24 13:52:52 +0100

    Fix whitespace error

--- client/src/App.js
+++ client/src/App.js
@@ -42,7 +42,7 @@ function App() {
         <footer className="py-5 bg-dark">
             <div className="container"><p class="m-0 text-center text-white">Copyright &copy; Your Website 2021</p></div>
         </footer>
-  </>
+    </>
   );
 }
 
