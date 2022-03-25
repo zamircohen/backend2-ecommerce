acd53f9a71c3f1507de8cc5cff9272c45465aab0
Christoffer Ekeroth <ekeroth.christoffer@gmail.com>
2022-03-25 16:23:00 +0100

    Fix indentation

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -14,7 +14,7 @@ const Home = () => (
     <section className="py-5">
         <div className="container px-4 px-lg-5 mt-5">
             <div className="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
-            { PRODUCTS.map(product => <ProductListItem key={product.sku} {...product} />) }
+                { PRODUCTS.map(product => <ProductListItem key={product.sku} {...product} />) }
             </div>
         </div>
     </section>
