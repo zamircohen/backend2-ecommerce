81af9860177b3223bf70e0d091d002bcb063da2d
Zamir Cohen <90824610+zamircohen@users.noreply.github.com>
2022-03-28 11:56:30 +0200

    Added CORS

--- server/app.js
+++ server/app.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const cors = require("cors")
 
 const { productRoutes } = require("./controllers/products.js")
 
 const app = express()
 const PORT = 5000;
+
+app.use(cors())
 
 app.get("/", (req, res) => {
   res.send("Hello world!");
